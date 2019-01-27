c28d4c281f5e5e9eb274ba216b25f26eb0c81bd9
12ColcloughJ <12colcloughj@lytchett.org.uk>
2019-01-27 11:31:56 +0000

    Made the program more user friendly.
    Added a help menu that you can open with 'h', and assigned each sort to a number on the keyboard.

--- sortVisual.go
+++ sortVisual.go
@@ -12,6 +12,9 @@ var (
 	SCREEN_WIDTH  float32 = 1600
 	SCREEN_HEIGHT float32 = 800
 
+	helpW int32 = 533
+	helpH int32 = int32(math.Floor(float64(SCREEN_HEIGHT)/4))
+
 	//violet rl.Color = NewColor(61, 38, 69, 255)
 	//raspberry rl.Color = NewColor(131, 33, 97, 255)
 	//coral rl.Color = NewColor(218, 65, 103, 255)
@@ -364,6 +367,44 @@ func (a *AnimArr) RunShowcase() {
 	a.CurrentText = ""
 }
 
+
+func displayHelp() {
+	rl.DrawRectangle(20, 60, helpW, helpH, rl.LightGray)
+
+	v1, v2, v3, v4 := rl.NewVector2(20, 60), rl.NewVector2(float32(helpW)+20, 60), rl.NewVector2(20, float32(helpH)+60), rl.NewVector2(float32(helpW)+20, float32(helpH)+60) // All 4 verticies of square
+
+	// BORDER
+	rl.DrawLineEx(v1, v2, 2, rl.Red) // Top line
+	rl.DrawLineEx(v3, v4, 2, rl.Red) // Bottom line
+	rl.DrawLineEx(v1, v3, 2, rl.Red)  // Left line
+	rl.DrawLineEx(v2, v4, 2, rl.Red)
+
+	// Information
+	var (
+		g1x int32 = 30
+		g2x int32 = helpW/2  // Guidelines
+		fontSize int32 = 20
+	)
+	rl.DrawText("Key", g1x, 70, fontSize+2, rl.Black)     // Headers
+	rl.DrawText("Function", g2x, 70, fontSize+2, rl.Black)
+
+	rl.DrawText("1,2...9", g1x, 110, fontSize, rl.Black)
+	rl.DrawText("Do various sorts.", g2x, 110, fontSize, rl.Black)
+
+	rl.DrawText("'s'", g1x, 140, fontSize, rl.Black)
+	rl.DrawText("Shuffle the data.", g2x, 140, fontSize, rl.Black)
+
+	rl.DrawText("'r'", g1x, 170, fontSize, rl.Black)
+	rl.DrawText("Reverse the data.", g2x, 170, fontSize, rl.Black)
+
+	rl.DrawText("'p'", g1x, 200, fontSize, rl.Black)
+	rl.DrawText("Run the showcase.", g2x, 200, fontSize, rl.Black)
+
+	rl.DrawText("'l'", g1x, 230, fontSize, rl.Black)
+	rl.DrawText("Sort the data instantly.", g2x, 230, fontSize, rl.Black)
+}
+
+
 func main() {
 	rl.InitWindow(int32(SCREEN_WIDTH), int32(SCREEN_HEIGHT), "Sort Visualiser")
 	rl.SetTargetFPS(144)
@@ -371,25 +412,44 @@ func main() {
 	anim := AnimArr{}
 	anim.Init(2, true, false, 2)  // Input line thickness, if it is linear, and if it is color only here
 
+	helpOpen := false
+
 	for !rl.WindowShouldClose() {
-		if rl.IsKeyPressed(rl.KeyR) && !anim.Shuffling && !anim.Sorting && !anim.Showcase {  // When 'r' is pressed, shuffle the array.
-			go anim.Shuffle(2, true)
-		} else if rl.IsKeyPressed(rl.KeyS) && !anim.Sorting && !anim.Shuffling && !anim.Showcase {  // When 's' is pressed, sort the array.
-			anim.DoSort("quick")
-		} else if rl.IsKeyPressed(rl.KeyL) && !anim.Sorting && !anim.Showcase {
-			anim.Data = regularQuickSort(anim.Data)
-			anim.Sorted = true
-		} else if rl.IsKeyPressed(rl.KeyI) && !anim.Showcase {
-			go func() {
-				anim.Reverse()
-			}()
-		} else if rl.IsKeyPressed(rl.KeyP) && !anim.Showcase && !anim.Sorting && !anim.Shuffling {
-			go anim.RunShowcase()
+		if !anim.Sorting && !anim.Shuffling && !anim.Showcase {
+			if rl.IsKeyPressed(rl.KeyS) {
+				go anim.Shuffle(2, true)
+
+			} else if rl.IsKeyPressed(rl.KeyOne) {
+				anim.DoSort("quick")
+			} else if rl.IsKeyPressed(rl.KeyTwo) {
+				anim.DoSort("bubble")
+			} else if rl.IsKeyPressed(rl.KeyThree) {
+				anim.DoSort("insertion")
+			} else if rl.IsKeyPressed(rl.KeyNine) {
+				anim.DoSort("bogo")
+			} else if rl.IsKeyPressed(rl.KeyL) {
+				anim.Data = regularQuickSort(anim.Data)
+				anim.Sorted = true
+			} else if rl.IsKeyPressed(rl.KeyR) {
+				go func() {
+					anim.Reverse()
+				}()
+			} else if rl.IsKeyPressed(rl.KeyP) {
+				go anim.RunShowcase()
+			}
+		}
+
+		if rl.IsKeyPressed(rl.KeyH) { // Open H
+			helpOpen = !helpOpen
 		}
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 		anim.Draw()
+
+		if helpOpen {
+			displayHelp()
+		}
 
 		rl.EndDrawing()
 	}
